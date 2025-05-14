56c1e1dd0d039d98c47befd0239534802921a58e
PushpaKumara89 <kpushpakumarajd@gmail.com>
2025-05-14 22:24:11 +0530

    env spring boot app initializing

---
+++ src/main/java/com/appk/envwithspringboot/EnvWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.appk.envwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appk/envwithspringboot/EnvWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appk.envwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
